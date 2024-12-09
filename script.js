196b5a2627b027a002d3d3168de8bd383cfce0bc
Selvia <sn949@njit.edu>
2024-12-08 22:58:21 -0800

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let characters = ["Harry", "Hermione", "Ron", "Draco", "Luna"]
+for (i = 5; i < characters.length; i++)
+    {
+    console.log(characters[i]);
+    }
+    
+
+let firstName = "Harry";
+let house = "Gryffindor";
+print("Welcome, {firstName} of {house}")
+
